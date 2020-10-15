55062f0e77b7d7bac2cc4eb9060ab530e86d680c
Dunieski Otano <dunieskior@gmail.com>
2020-10-15 15:05:20 -0400

    Added more code to Functions

--- Functions/functions.py
+++ Functions/functions.py
@@ -56,3 +56,40 @@ print(f"Phone = {phone}")
 
 studentName = input("What is your name? >> ")
 print("Welcome, {}". format(studentName))
+
+def personInfo(name, lname, age, phoneNum, PIN, school):
+    print(f"""
+    First Name:.............{name}
+    Last Name:..............{lname}
+    Age:....................{age}
+    Phone:..................{phoneNum}
+    PIN:....................{PIN}
+    School:.................{school}
+    """)
+
+# starts here
+os.system('clear')
+print("******** Multiple Values for Function **********")
+name = input("What is your first name? >> ")
+lname = input("What is your last name? >> ")
+age = input("What is your age? >> ")
+phone = input("What is your phone? >> ") 
+pin = input("What is your PIN? >> ")
+school = input("What is your school? >> ")
+
+personInfo(name, lname, age, phone, pin, school)
+
+#array = ("Dunieski", "Otano", 44, "NCI", 1465, 34.2, True)
+
+#index =>  0    1    2   3    4    5    6 => size - 1 
+array1=[   87,  10,  2,  46,  22,  19,  66 ]# => size is 7
+#size =>   1    2    3   4    5    6    7
+
+cars = ["BMW", "Toyota", "Mercedes"]
+cars.sort()
+print(cars)
+
+
+numbers = [6, 3, 8, 1, 2, 8, 3]
+numbers.sort()
+print(numbers)
